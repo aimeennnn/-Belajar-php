6cef256e9170c9dddcf8a98d2e4bc5ef0d705892
aimeennnn <muh.aiman21304@gmail.com>
2023-09-02 22:03:18 +0700

    first commit

---
+++ tipedata.php
@@ -0,0 +1,23 @@
+<?php
+$x = 5985;
+var_dump($x);
+echo "<br>";
+$x = -345;
+var_dump($x);
+echo "<br>";
+$x = 0x8c;
+var_dump($x);
+echo "<br>";
+$x = 047;
+var_dump($x);
+$x = 10.365;
+var_dump($x);
+echo "<br>";
+$x = 24e3;
+var_dump($x);
+echo "<br>";
+$x = 8E-5;
+var_dump($x);
+echo strlen("Hello World!!");
+echo strpos("Hello World!","World");
+?>
---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+$nim=" E4122163";
+$nama=" Muhammad Aiman";
+$prodi=" Teknik Informatika";
+echo " Selamat Praktikum";
+echo $nim;
+echo $nama;
+echo $prodi;
+?>
